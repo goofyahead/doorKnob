8b78acc1b04060d516751911cbaf74e6a1ca067d
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2016-01-24 22:03:59 +0100

    change gpio ports

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var Gpio = require('onoff').Gpio;
 
-var step = new Gpio(3,'out');
-var direction = new Gpio(5,'out');
+var step = new Gpio(18,'out');
+var direction = new Gpio(16,'out');
 
 
 // Toggle the state of the LED on GPIO #14 every 200ms.
 // Here synchronous methods are used. Asynchronous methods are also available.
 direction.writeSync(0);
 iv = setInterval(function () {
-  step.writeSync(step.readSync() ^ 1); // 1 = on, 0 = off :)
+	console.log('stat to: ', step.readSync());
+  	step.writeSync(step.readSync() ^ 1); // 1 = on, 0 = off :)
 }, 200);
 
 // Stop blinking the LED and turn it off after 5 seconds.
