3f41b2357f57a8f3dc3c5de3e7f2610c99ecefa6
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2016-01-24 22:51:22 +0100

    timestamp and lib moment

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var Gpio = require('onoff').Gpio;
+var moment = require('moment');
 
 // BCM GPIO NOTATION
 var step = new Gpio(23,'out');
@@ -7,17 +8,20 @@ var direction = new Gpio(24,'out');
 
 // Toggle the state of the LED on GPIO #14 every 200ms.
 // Here synchronous methods are used. Asynchronous methods are also available.
+
+console.log('Ts: ', moment().format('mm:ss');
 direction.writeSync(0);
 iv = setInterval(function () {
 	console.log('stat to: ', step.readSync());
   	step.writeSync(step.readSync() ^ 1); // 1 = on, 0 = off :)
-}, 200);
+}, 50);
 
 // Stop blinking the LED and turn it off after 5 seconds.
 setTimeout(function () {
   clearInterval(iv); // Stop blinking
   step.writeSync(0);  // Turn LED off.
-}, 5000);
+  console.log('Ts: ', moment().format('mm:ss');
+}, 10000);
 
 
 function exit() {
